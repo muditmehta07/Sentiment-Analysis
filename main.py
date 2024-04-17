8faec831b0bb6f397dfd1a98dfe12f4fe35bf318
Mudit Mehta <70369287+muditmehta07@users.noreply.github.com>
2024-04-18 00:06:07 +0530

    not 13 lines anymore

--- main.py
+++ main.py
@@ -4,10 +4,37 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import torch
 import torch.nn.functional as F
 from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-classifier = pipeline("sentiment-analysis")
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+classifier = pipeline("sentiment-analysis", model=model_name)
 
-while True:
-    INPUT = input("> ")
-    res = classifier(INPUT)
-    print(res)
+sample_data = []
+for i in range(1, 3):
+    INPUT = input(f"> Enter sentence {i} :")
+    sample_data.append(INPUT)
+
+tokens = tokenizer.tokenize(sample_data)
+token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+X_train = sample_data
+
+batch = tokenizer(X_train, padding=True, truncation=True, max_length=512, return_tensors="pt")
+
+with torch.no_grad():
+    outputs = model(**batch, labels=torch.tensor([1, 0]))
+    print(outputs)
+    predictions = F.softmax(outputs.logits, dim=1)
+    print(predictions)
+    labels = torch.argmax(predictions, dim=1)
+    print(labels)
+    labels = [model.config.id2label[label_id] for label_id in labels.tolist()]
+    print(labels)
+
+save_dir = "saved"
+tokenizer.save_pretrained(save_dir)
+model.save_pretrained(save_dir)
+tokenizer = AutoTokenizer.from_pretrained(save_dir)
+model = AutoModelForSequenceClassification.from_pretrained(save_dir)
